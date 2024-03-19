5dcc17cf198c2d205414ab5fcc28be0964813847
Yulei Chen <yulei_chen@epam.com>
2024-03-19 18:07:50 +0800

    Init with nextjs and codehike (remember to install @mdx-js/react)

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const { remarkCodeHike } = require('@code-hike/mdx')
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [[remarkCodeHike, { theme: 'nord' }]],
+  },
+})
+
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '@code-hike/mdx/dist/index.css'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
