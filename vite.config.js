d8a0d11bb30dc8f07e4743ed5002a99213d80de4
Yulei Chen <yulei_chen@epam.com>
2024-06-09 13:17:46 +0800

    build(vite): fix files in dist/assets not founded

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Fix files in `dist/assets` not founded in deployment env (github pages)
+  // https://github.com/orgs/community/discussions/61478#discussioncomment-6516001
+  base: '/react-router-tutorial',
   plugins: [react()],
 })
